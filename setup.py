889e321bfd67bf56aed902a98edab5ec5b5436f5
Nishant Jha <nishantjha360@gmail.com>
2021-08-26 05:10:11 +0530

    Fix Setup

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ import json
 
 login_dict = dict()
 login_dict["username"] = input("Username : ")
-login_dict["password"] = input("Password : ")
+login_dict["password"] = input("Password : ",)
 with open("./JSONfiles/login.json", "w") as login:                          #stores the login details to a json file
     json.dump(login_dict,login,indent=4)
 
-from tools import get_classes
-from selection import get_semester_classes
-from data import course_list
-from driver import driver
+from src.tools import get_classes,clear_screen
+from src.selection import get_semester_classes
+from src.data import course_list
+from src.driver import driver
 
 try:
     get_classes()
@@ -23,7 +23,8 @@ try:
     durations = dict()
     print("Duration you want to attend in minutes (Ex. : 60)")
     durations["class duration"] = int(input("Class Duration : "))
-    durations["lab duration"] = int(input("Lab Duration : "))
+    durations["lab duration"] = int(input("Lab Duration   : "))
+    clear_screen()
     with open("./JSONfiles/durations.json", "w") as duration_file:          #stores the duration details in a json file
         json.dump(durations, duration_file, indent=4)
 except:
