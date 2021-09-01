d0aa2c0bfea42a0cc3c2938018ab0b39483200c5
Nishant Jha <nishantjha360@gmail.com>
2021-09-01 19:39:40 +0530

    Add tkinter for Windows

--- src/selection.py
+++ src/selection.py
@@ -1,22 +1,45 @@
-#all the fucntions used for command line selection
+from os import name
+from .data import course_dict,todays_courses
 
-from simple_term_menu import TerminalMenu
-from .data import todays_courses,course_dict
-def select_todays_classes():
+if name == 'nt':
+    from tkinter import Tk,IntVar,Checkbutton,Button,TOP,W
     
-    terminal_menu = TerminalMenu(
-        course_dict,
-        title="-------------------- Select Today's Classes --------------------",
-        multi_select=True,
-        show_multi_select_hint=True,
-        multi_select_cursor_style=("fg_blue", "bold"),
-        menu_cursor_style=("fg_purple",),
-        status_bar_style=("fg_cyan",),
-        multi_select_select_on_accept=False,
-        clear_screen=True
-    )
-    terminal_menu.show()
-    
-    for course in course_dict:
-        if course in terminal_menu.chosen_menu_entries:
-            todays_courses.append(course)
+    def select_todays_classes():
+        root = Tk()
+        course_variable = dict()
+
+        def submit():
+            for course in course_dict():
+                if course_variable[course].get() == 1:
+                    todays_courses.append(course)
+            root.destroy()
+
+        for course in course_dict:
+            course_variable[course] = IntVar()
+            l = Checkbutton(root, text=course, variable=course_variable[course] )
+            l.pack(side=TOP, anchor=W)
+        submit = Button(root,text="Submit",command=submit)
+        submit.pack()
+
+        root.mainloop()
+
+else:
+    from simple_term_menu import TerminalMenu
+    def select_todays_classes():
+        
+        terminal_menu = TerminalMenu(
+            course_dict,
+            title="-------------------- Select Today's Classes --------------------",
+            multi_select=True,
+            show_multi_select_hint=True,
+            multi_select_cursor_style=("fg_blue", "bold"),
+            menu_cursor_style=("fg_purple",),
+            status_bar_style=("fg_cyan",),
+            multi_select_select_on_accept=False,
+            clear_screen=True
+        )
+        terminal_menu.show()
+        
+        for course in course_dict:
+            if course in terminal_menu.chosen_menu_entries:
+                todays_courses.append(course)
--- stp/selection.py
+++ stp/selection.py
@@ -1,23 +1,45 @@
-#all the fucntions used for command line selection
-
-from simple_term_menu import TerminalMenu
+from os import name
 from .data import course_dict
 
-def select_semester_classes():
+if name == 'nt':
+    from tkinter import Tk,IntVar,Checkbutton,Button,TOP,W
     
-    terminal_menu = TerminalMenu(
-        course_dict,
-        title="-------------------- Select this Semester Classes --------------------",
-        multi_select=True,
-        show_multi_select_hint=True,
-        multi_select_cursor_style=("fg_blue", "bold"),
-        menu_cursor_style=("fg_purple",),
-        status_bar_style=("fg_cyan",),
-        multi_select_select_on_accept=False,
-        clear_screen=True
-    )
-    terminal_menu.show()
-    
-    for course in course_dict.copy():
-        if course not in terminal_menu.chosen_menu_entries:
-            del course_dict[course]
+    def select_semester_classes():
+        root = Tk()
+        course_variable = dict()
+
+        def submit():
+            for key in course_dict.copy():
+                if course_variable[key].get() == 0:
+                    del course_dict[key]
+            root.destroy()
+
+        for course in course_dict:
+            course_variable[course] = IntVar()
+            l = Checkbutton(root, text=course, variable=course_variable[course] )
+            l.pack(side=TOP, anchor=W)
+        submit = Button(root,text="Submit",command=submit)
+        submit.pack()
+
+        root.mainloop()
+
+else:
+    from simple_term_menu import TerminalMenu
+
+    def select_semester_classes():
+        terminal_menu = TerminalMenu(
+            course_dict,
+            title="-------------------- Select this Semester Classes --------------------",
+            multi_select=True,
+            show_multi_select_hint=True,
+            multi_select_cursor_style=("fg_blue", "bold"),
+            menu_cursor_style=("fg_purple",),
+            status_bar_style=("fg_cyan",),
+            multi_select_select_on_accept=False,
+            clear_screen=True
+        )
+        terminal_menu.show()
+        
+        for course in course_dict.copy():
+            if course not in terminal_menu.chosen_menu_entries:
+                del course_dict[course]
